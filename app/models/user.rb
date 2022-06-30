92ab8e3f7e48228be5a9b2d2fbed2451cca6f6c1
Andrey Timakhovich <stewart96.at@gmail.com>
2022-06-30 15:34:27 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  # constraint for usename length and uniqueness
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 40 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # constraint for user's email length and uniqueness
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false }, 
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
