b0c88b77a82f4491842bb8e9d84366ef5a8a1425
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-01 15:30:51 +0300

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # method for creating new users
   def create
-    byebug
+     # creating new user with params(username, email, password)
+     @user = User.new(user_params)
+ 
+     # when the new user was created parameters created_at, updated_at sets automatically (see users table in schema.db)
+     if @user.save
+       flash[:notice] = "Welcome, #{@user.username}. You have successfully signed up!"
+       # redirect to the new article show page
+       redirect_to articles_path
+     else
+       # redirecting to the new page if the user wasn't created
+       render 'new'
+     end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
