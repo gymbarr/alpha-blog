cc87dd443aee104a4f6f3947f0ca5daae05f1200
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-05 14:24:58 +0300

    add authentication helper methods and navbar fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   # method gets the User instance by user's id
   # it's used for displaying a user profile
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -18,12 +19,10 @@ class UsersController < ApplicationController
 
   # the method used for editing a user profile
   def edit
-    @user = User.find(params[:id])
   end
 
   # method for updating users
   def update
-    @user = User.find(params[:id])
     # updating new article with title and description params
     # when the article was updated parameterupdated_at updated automatically
     if @user.update(user_params)
@@ -41,18 +40,26 @@ class UsersController < ApplicationController
      # creating new user with params(username, email, password)
      @user = User.new(user_params)
  
-     # when the new user was created parameters created_at, updated_at sets automatically (see users table in schema.db)
-     if @user.save
-       flash[:notice] = "Welcome, #{@user.username}. You have successfully signed up!"
-       # redirect to the articles page
-       redirect_to articles_path
-     else
-       # redirecting to the new page if the user wasn't created
-       render 'new'
-     end
+    # when the new user was created parameters created_at, updated_at sets automatically (see users table in schema.db)
+    if @user.save
+      # sign in user when he signed up
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.username}. You have successfully signed up!"
+      # redirect to the articles page
+      redirect_to articles_path
+    else
+      # redirecting to the new page if the user wasn't created
+      render 'new'
+    end
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
